c72c8b62e0c369ae45e60b7c0ecf52a327782422
cpg314 <44120267+cpg314@users.noreply.github.com>
2024-02-21 14:05:09 +0100

    feat(py): Add function to read a chunk from pre-loaded PNG bytes

--- py/src/lib.rs
+++ py/src/lib.rs
@@ -1,14 +1,37 @@
+use std::io::{Cursor, Read, Seek};
 use std::path::PathBuf;
 
 use pyo3::{exceptions::PyRuntimeError, prelude::*, types::PyBytes, types::PyString};
 
+/// Reads the ancillary chunk with the given `name` from the PNG `bytes`.
+#[pyfunction]
+fn read_chunk_from_png_bytes<'a>(
+    py: Python<'a>,
+    bytes: &'a PyBytes,
+    name: &'a PyString,
+) -> PyResult<&'a PyBytes> {
+    let cursor = Cursor::new(bytes.as_bytes());
+    let decoder = png_achunk::Decoder::from_reader(cursor);
+    read_chunk_from_decoder(py, decoder, name)
+}
+
+/// Reads the ancillary chunk with the given `name` from the PNG file `filename`.
 #[pyfunction]
 fn read_chunk<'a>(
     py: Python<'a>,
     filename: &'a PyString,
     name: &'a PyString,
 ) -> PyResult<&'a PyBytes> {
-    let chunks = png_achunk::Decoder::from_file(filename.extract::<PathBuf>()?)?
+    let decoder = png_achunk::Decoder::from_file(filename.extract::<PathBuf>()?)?;
+    read_chunk_from_decoder(py, decoder, name)
+}
+
+fn read_chunk_from_decoder<'a, R: Read + Seek>(
+    py: Python<'a>,
+    mut decoder: png_achunk::Decoder<R>,
+    name: &'a PyString,
+) -> PyResult<&'a PyBytes> {
+    let chunks = decoder
         .decode_ancillary_chunks()
         .map_err(|e| PyRuntimeError::new_err(format!("Failed to decode PNG chunks: {}", e)))?;
     let name: String = name.extract()?;
@@ -24,6 +47,7 @@ fn read_chunk<'a>(
 #[pymodule]
 #[pyo3(name = "png_achunk")]
 fn module(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(read_chunk_from_png_bytes, m)?)?;
     m.add_function(wrap_pyfunction!(read_chunk, m)?)?;
     Ok(())
 }
